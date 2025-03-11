2fddc8c67e9e7647535e41b70cc65fff6bcfc4b9
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-12 00:41:07 +0530

    changes in java script

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ let buffer = "0";
 let previousoperator;
 
 const screen= document.querySelector( '.screen');
+function buttonclick(value){
+    if(isNaN(value)){
+        handleSymble(value);
+}else{
+    handleSymble(value);
+}
+screen.innerTxt=buffer;
+}
+function handleSymbole(symbole){
+    switch(symbole){
+        case 'C':
+        buffer= "0";
+        runningtotal=0;
+        break;
+        
+
+    }
+
+}
