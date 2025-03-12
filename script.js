72ae115a2d916f8ac19c1cce156d7f4916a0aed3
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-12 19:08:40 +0530

    add mathamatical functions

--- script.js
+++ script.js
@@ -11,18 +11,50 @@ function buttonclick(value){
 }
 screen.innerTxt=buffer;
 }
-function handleSymbole(symbole){
-    switch(symbole){
+function handleSymbole(symbol){
+    switch(symbol){
         case 'C':
         buffer= "0";
         runningtotal=0;
         break;
+
         case'=':
         if(previousoperator === null){
             return
         }
         flushOperation(parseInt(buffer));
+        previousoperator =null;
+        buffer = runningtotal;
+        runningtotal =0;
+        break;
 
+        case '←':
+            if(buffer.length ===1){
+                buffer= '0';
+            }  else{
+                buffer = buffer.toString(0,buffer.length =1);
+            }
+            break;
+            case '+':
+            case '-':
+            case '×':
+            case '÷':
+                handlemath(Symbol);
+                break;   
     }
-
 }
+function handlemath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningtotal ===0){
+        runningtotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
+    previousoperator = symbol;
+    buffer = '0';
+}
+
